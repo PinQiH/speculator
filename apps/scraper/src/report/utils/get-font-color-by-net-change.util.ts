ae3a017a4cde616b003f9ce7184d57208410d45f
PinQiH <83577156+PinQiH@users.noreply.github.com>
2023-09-12 15:19:54 +0800

    Create get-font-color-by-net-change.util.ts

---
+++ apps/scraper/src/report/utils/get-font-color-by-net-change.util.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Color } from '../enums';
+
+export function getFontColorByNetChange(netChange: number): Color {
+    if (netChange > 0) return Color.Up;
+    if (netChange < 0) return Color.Down;
+    return Color.Unchanged;
+}
