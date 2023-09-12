f28172e3b3f1b4ae5f8f61a1ae8028187a8ed32d
PinQiH <83577156+PinQiH@users.noreply.github.com>
2023-09-12 15:28:22 +0800

    Create get-market-name.util.ts

---
+++ libs/common/src/utils/get-market-name.util.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Market } from '../enums';
+
+export function getMarketName(market: Market) {
+    const markets = {
+        [Market.TSE]: '上市',
+        [Market.OTC]: '上櫃',
+        [Market.ESB]: '興櫃一般板',
+        [Market.TIB]: '臺灣創新板',
+        [Market.PSB]: '興櫃戰略新板',
+    }
+    return markets[market];
+}
